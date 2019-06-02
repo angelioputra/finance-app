acfdbad95fc4087045ed63598e39719998aa9ade
Angelio Putra <putralio.angelos@gmail.com>
2019-06-02 23:58:59 +0700

    Redeploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const
   express = require('express'),
   serveStatic = require('serve-static'),
+  history = require('connect-history-api-fallback'),
   port = process.env.PORT || 5000
 
 const app = express()
